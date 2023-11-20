630afaca83edc207c869e45cfc13c9f6dbda1206
srtoxyc <ivi1000vm@gmail.com>
2023-11-20 19:07:28 +0100

    Session implemented #2.

--- src/main/kotlin/org/gycoding/model/database/DBFactory.kt
+++ src/main/kotlin/org/gycoding/model/database/DBFactory.kt
@@ -2,13 +2,11 @@ package org.gycoding.model.database
 
 object DBFactory {
     public val MODE_TEST: Int       = 0
-    public val MODE_SQLITE: Int     = 1
-    public val MODE_MYSQL: Int      = 2
+    public val MODE_MYSQL: Int      = 1
 
     public fun getDAO(mode: Int): DBDAO? {
         return when(mode) {
             MODE_TEST -> null
-            MODE_SQLITE -> SQLiteDAO()
             MODE_MYSQL -> MySQLDAO()
             else -> null
         }
